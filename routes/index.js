af162b64e8fab24a8fbfb35df3ce1c7b8ffca79e
Riny George <rinygeorge@Anoops-MacBook-Pro-2.local>
2020-09-29 20:22:33 -0700

    Changes

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@
 // Assumes DATABASE_URL in the heroku configs points to your PostGres database
 // setups the url /db to connect to database and does a query from a table test_table
 var pg = require('pg');
-app.get('/db', function (request, response) {
+app.get('/', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
       { console.error(err); response.send("Error " + err); }
       else
-      { response.render('pages/db', {results: result.rows} ); }
+      { response.render('db', {results: result.rows} ); }
     });
   });
 });
