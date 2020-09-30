5f0db6823b2a59da3092d1a5da988df3bbaffaaf
Riny George <rinygeorge@Anoops-MacBook-Pro-2.local>
2020-09-29 20:32:42 -0700

    Changes

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM test_table');
     const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
+    res.render('db', results );
     client.release();
   } catch (err) {
     console.error(err);
