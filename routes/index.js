403bafc713a01c39de940ede85858d47168e2987
Riny George <rinygeorge@Anoops-MacBook-Pro-2.local>
2020-10-04 22:43:42 -0700

    Added screenshots of appl;ication

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'An exercise using Node, Express.Postgres and Heroku' });
+  res.render('index', { title: 'An Exercise using Node, Express, Postgres and Heroku' });
 });
 
 module.exports = router;
