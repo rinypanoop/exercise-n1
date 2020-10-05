ad7af87a7d807ae004311f5131ebe5641c30649c
Riny George <rinygeorge@Anoops-MacBook-Pro-2.local>
2020-10-04 22:20:11 -0700

    Data added to books table

--- routes/db.js
+++ routes/db.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 
 
 /* GET home page. */
-router.get('/', async (req, res) => {
+router.get('/db', async (req, res) => {
   try {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM books');
