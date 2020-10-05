93decbab90e44194f5b7c23da1986d9b2d1e542a
Riny George <rinygeorge@Anoops-MacBook-Pro-2.local>
2020-10-04 22:26:10 -0700

    Added tabke head

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-
 /* GET home page. */
-router.get('/', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM books');
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('db', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-})
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
